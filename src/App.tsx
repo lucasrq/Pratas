a2f28107ca788a6b5f0eea4bc231fd8a5201a601
lucas <lucasroque545@gmail.com>
2025-01-12 23:27:04 -0300

    style: adiciona contêiner estilizado para o componente App e corrige overflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Carrinho from "./Container/Carrinho/Carrinho"
-import Header from "./Container/Header/header"
+import Carrinho from "./Container/Carrinho/Carrinho";
+import Header from "./Container/Header/header";
 
-function App(){
-    return(
-        <>
-            <Header />
-            <Carrinho/>
-        </>
-    )
+function App() {
+  return (
+    <div style={{ overflow: 'hidden', width: '100%', height: '100vh', position: 'relative' }}>
+      <Header />
+      <Carrinho />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/appcss.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const APPs = styled.div`
+    *{
+        overflow-x:hidden;
+    }
+`
+export default APPs
